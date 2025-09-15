68fb372cbecb7e8cbac840eb175c7658fa70da30
和田祐樹 <wadayuuki@macbookpro.lan>
2025-09-15 22:31:48 +0900

    DOMContentLoadedで読み込ませる。

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "@src/App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+// DOMの読み込み完了を待つ
+document.addEventListener("DOMContentLoaded", () => {
+  const element = document.getElementById("root");
+  if (element) {
+    ReactDOM.createRoot(element).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+});
