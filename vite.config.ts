012dca6f3417731b8a6acbaba0269adedccac94a
和田祐樹 <wadayuuki@macbookpro.lan>
2025-09-16 09:14:14 +0900

    assetの設置とlinterの変更に伴う修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@src": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
+      '@': path.resolve(__dirname, './src'),
+      '@src': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './assets'),
+      '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
   css: {
-    postcss: "./postcss.config.cjs",
+    postcss: './postcss.config.cjs',
     preprocessorOptions: {
       css: {
         charset: false,
@@ -24,24 +24,24 @@ export default defineConfig({
     },
   },
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 3000,
     watch: {
       usePolling: true,
     },
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-            return "assets/css/[name]-[hash][extname]";
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/css/[name]-[hash][extname]'
           }
-          return "assets/[name]-[hash][extname]";
+          return 'assets/[name]-[hash][extname]'
         },
       },
     },
   },
-});
+})
