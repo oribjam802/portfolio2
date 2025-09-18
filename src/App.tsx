f8c1d651e878679501b6ea9f76227f9ab199f440
和田祐樹 <wadayuuki@wadayuukinombp.lan>
2025-09-18 23:00:35 +0900

    footer削除

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Header from '@components/Header'
 import Home from '@pages/Home'
 import Projects from '@pages/Projects'
-import Footer from '@components/Footer'
 
 const App: React.FC = () => {
   return (
