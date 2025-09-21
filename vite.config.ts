8ebcaee2388d318f5db0352306563aeb4988a893
和田祐樹 <wadayuuki@wadayuukinoMacBook-Pro.local>
2025-09-22 00:11:22 +0900

    相対パスで

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   // GitHub Pages用のベースパス設定
-  base: process.env.NODE_ENV === 'production' ? '/portfolio2/' : '/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
