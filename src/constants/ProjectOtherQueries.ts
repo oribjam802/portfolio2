7ab6612dbcfe14dc95b60716ed902db6b3a3e97a
和田祐樹 <wadayuuki@wadayuukinoMacBook-Pro.local>
2025-09-22 00:18:39 +0900

    画像パス変更

--- src/constants/ProjectOtherQueries.ts
+++ src/constants/ProjectOtherQueries.ts
@@ -2,14 +2,14 @@ import { ProjectOtherType } from '@src/types/ProjectsQueriesType'
 
 const ProjectOtherQueries: ProjectOtherType = {
   images: [
-    'assets/images/img/other/img-aqualox-toppage--sp.png',
-    'assets/images/img/other/img-aqualox-toppage.png',
-    'assets/images/img/other/img-kanpaikobe-toppage--sp.png',
-    'assets/images/img/other/img-kanpaikobe-toppage.png',
-    'assets/images/img/other/img-loccitane-toppage--sp.png',
-    'assets/images/img/other/img-loccitane-toppage.png',
-    'assets/images/img/other/img-seibo-back.png',
-    'assets/images/img/other/img-seibo-top.png',
+    '/assets/images/img/other/img-aqualox-toppage--sp.png',
+    '/assets/images/img/other/img-aqualox-toppage.png',
+    '/assets/images/img/other/img-kanpaikobe-toppage--sp.png',
+    '/assets/images/img/other/img-kanpaikobe-toppage.png',
+    '/assets/images/img/other/img-loccitane-toppage--sp.png',
+    '/assets/images/img/other/img-loccitane-toppage.png',
+    '/assets/images/img/other/img-seibo-back.png',
+    '/assets/images/img/other/img-seibo-top.png',
   ],
 }
 
